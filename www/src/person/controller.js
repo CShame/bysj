0df047281a496e456813c968c2f2aa108097c202
CShame <1138158134@qq.com>
2019-04-26 14:42:02 +0800

    add person page

--- www/src/person/controller.js
+++ www/src/person/controller.js
@@ -1,11 +1,28 @@
 angular.module('starter.controllers')
-    .controller('PersonCtrl', ['$scope','$state','CacheFactory', function ($scope, $state,CacheFactory) {
+    .controller('PersonCtrl', ['$scope', '$state', 'MemoryFactory','CacheFactory', function ($scope, $state, MemoryFactory,CacheFactory) {
+
+        $scope.person = MemoryFactory.getCurrentUserInfo();
+
+        var defaultPhoto = 'asset/img/default.png';
 
 
-            $scope.exit = function () {
-                CacheFactory.removeAll();
-                setTimeout(function(){
-                  ionic.Platform.exitApp();
-                }, 100);
+        init();
+        function init() {
+            if($scope.person.img){
+                $scope.personStyle = {
+                    imgStyle:{'background-image':'url('+ $scope.person.img +')'},
+                }
+            } else {
+                $scope.personStyle = {
+                    imgStyle:{'background-image':'url('+defaultPhoto+')'},
+                }
             }
+        }
+
+        $scope.exit = function () {
+            CacheFactory.removeAll();
+            setTimeout(function () {
+                ionic.Platform.exitApp();
+            }, 100);
+        }
     }]);
